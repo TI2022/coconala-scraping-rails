2d4b0c57561d8c23e35918fd0a298f8af2a66cdf
iijima-tatsunori <himekawamaki45@gmail.com>
2022-07-05 19:06:18 +0900

    fixed seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,34 @@
-User.create!(name: "管理者",
-             email: "admin@email.com",
-             password: "admin01*+",
-             password_confirmation: "password",
-             admin: true)
-
-User.create!(name: "準管理者",
-            email: "sub_admin@email.com",
-            password: "subadmin01*+",
-            password_confirmation: "password",
-            accounting: true)
-
-User.create!(name: "guestユーザー",
-            email: "guest@email.com",
-            password: "guest",
-            password_confirmation: "guest")
-
-User.create!(name: "一般ユーザー",
-             email: "general1@email.com",
-             password: "general01*+",
-             password_confirmation: "password"
-             )
+User.find_or_create_by(
+[
+  {
+    id: 1,
+    name: "管理者",
+    email: "admin@email.com",
+    password: "admin01*+",
+    password_confirmation: "admin01*+",
+    admin: true
+  },
+  {
+    id: 2,
+    name: "準管理者",
+    email: "sub_admin@email.com",
+    password: "subadmin01*+",
+    password_confirmation: "subadmin01*+",
+    accounting: true
+  },
+  {
+    id: 3,
+    name: "guestユーザー",
+    email: "guest@email.com",
+    password: "guest",
+    password_confirmation: "guest"
+  },
+  {
+    id: 4,
+    name: "一般ユーザー",
+    email: "general1@email.com",
+    password: "general01*+",
+    password_confirmation: "general01*+"
+  }
+]
+)
