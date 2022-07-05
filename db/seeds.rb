beb15e319c7bb5238b184751bb8409bd320c727e
iijima-tatsunori <himekawamaki45@gmail.com>
2022-07-05 23:03:21 +0900

    fix seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,21 @@
-User.find_or_create_by(
+User.create!(
       name: "管理者",
       email: "admin@email.com",
       password: "admin01*+",
       password_confirmation: "admin01*+",
       admin: true)
-User.find_or_create_by(
+User.create!(
       name: "準管理者",
       email: "sub_admin@email.com",
       password: "subadmin01*+",
       password_confirmation: "subadmin01*+",
       accounting: true)
-User.find_or_create_by(
+User.create!(
       name: "guestユーザー",
       email: "guest@email.com",
       password: "guest",
       password_confirmation: "guest")
-User.find_or_create_by(
+User.create!(
       name: "一般ユーザー",
       email: "general1@email.com",
       password: "general01*+",
